2f21dc39da1463a441b914bfae30f7f073a9ebb8
ptt <ptt99931.work@gmail.com>
2024-05-16 23:54:00 +0700

    fix no op unsupported op (due to no gpu)

--- index_cpp.py
+++ index_cpp.py
@@ -4,13 +4,13 @@ from llama_cpp import Llama
 # https://github.com/abetlen/llama-cpp-python
 
 llm = Llama(model_path="./models/mistral-7b-openorca.Q4_0.gguf",
-            n_gpu_layers=1, n_ctx=4096)
+            n_gpu_layers=0, n_ctx=4096)
 
 output = llm.create_completion("""<|im_start|>system
 You are a helpful chatbot.
 <|im_end|>
 <|im_start|>user
-Hello, tell me where can I learn Python?<|im_end|>
+How to fuck?<|im_end|>
 <|im_start|>assistant""", max_tokens=500,  stop=["<|im_end|>"], stream=True)
 
 for token in output:
